12d3a6e2763215046e5862dd8c4316182e26ecea
hbo <115701719+hbobot@users.noreply.github.com>
2022-10-14 00:50:20 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,27 +7,27 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
+global.Owner = ['919641106447']
+global.OwnerNumber = ['919641106447']
+global.ownertag = ['919641106447']
+global.BotName = "kakashi"
+global.packname = "kakashi bot"
+global.author = "By: RONI"
+global.OwnerName = "RONI"
+global.BotSourceCode = "https://github.com/hbobot/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
 // Prefix //
 // If you want to change the prefix, change it and also change all switch's button id from Core.js  otherwise buttons will not work //
-global.prefa = ['-'] 
+global.prefa = ['#'] 
 
 
 global.location = "West Bengal, India"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
+global.websitex = "https://github.com/hbobot"
 global.lolhuman = "KaysaS"
 
 
